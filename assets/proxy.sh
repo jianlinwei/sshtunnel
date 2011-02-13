feb2b1d756b91237e1d1438b581f95e4a447632d
max.c.lv <max.c.lv@localhost>
2011-02-13 12:05:11 +0800

    commit for maintain

---
+++ assets/proxy.sh
@@ -0,0 +1,39 @@
+#!/system/bin/sh
+
+DIR=/data/data/org.sshtunnel/
+
+PATH=$DIR:$PATH
+
+case $1 in
+ start)
+
+echo "
+base {
+ log_debug = off;
+ log_info = off;
+ log = stderr;
+ daemon = on;
+ redirector = iptables;
+}
+" >$DIR/redsocks.conf
+
+   echo "
+redsocks {
+ local_ip = 127.0.0.1;
+ local_port = 8123;
+ ip = 127.0.0.1;
+ port = $2;
+ type = http-relay;
+} 
+" >>$DIR/redsocks.conf
+  echo "nameserver 8.8.8.8" > /etc/resolv.conf
+  $DIR/redsocks -p $DIR/redsocks.pid -c $DIR/redsocks.conf
+  ;;
+stop)
+  kill `cat $DIR/redsocks.pid`
+  
+  rm $DIR/redsocks.pid
+  
+  rm $DIR/redsocks.conf
+  ;;
+esac
