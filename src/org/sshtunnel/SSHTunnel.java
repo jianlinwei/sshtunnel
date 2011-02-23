e61691462f979c54a74a927dcea3c221faf3f481
max.c.lv <max.c.lv@localhost>
2011-02-23 15:15:11 +0800

    Starting 'droidbear' branch

--- src/org/sshtunnel/SSHTunnel.java
+++ src/org/sshtunnel/SSHTunnel.java
@@ -1,9 +1,12 @@
 package org.sshtunnel;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.ArrayList;
 
@@ -31,6 +34,9 @@ public class SSHTunnel extends PreferenceActivity implements
 
 	private static final String TAG = "SSHTunnel";
 	private static final String SERVICE_NAME = "org.sshtunnel.SSHTunnelService";
+	
+	Process sshProcess = null;
+	DataOutputStream sshOS = null;
 
 	private String host;
 	private int port;
@@ -190,7 +196,7 @@ public class SSHTunnel extends PreferenceActivity implements
 			isAutoSetProxyCheck.setChecked(false);
 			isAutoSetProxyCheck.setEnabled(false);
 		}
-
+		
 		if (!isWorked(SERVICE_NAME)) {
 			CopyAssets();
 			runRootCommand("chmod 777 /data/data/org.sshtunnel/iptables_g1");
@@ -198,6 +204,9 @@ public class SSHTunnel extends PreferenceActivity implements
 			runRootCommand("chmod 777 /data/data/org.sshtunnel/redsocks");
 			runRootCommand("chmod 777 /data/data/org.sshtunnel/proxy.sh");
 		}
+		
+		runCommand();
+
 	}
 
 	/** Called when the activity is closed. */
