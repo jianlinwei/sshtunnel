a20a3766d9223c6b897589f3c386834003909529
max.c.lv <max.c.lv@localhost>
2011-03-22 12:09:40 +0800

    fix for reconnect

--- src/org/sshtunnel/SSHTunnelService.java
+++ src/org/sshtunnel/SSHTunnelService.java
@@ -380,16 +380,8 @@ public class SSHTunnelService extends Service implements ConnectionMonitor {
 			if (reason.getMessage().contains(
 					"There was a problem during connect")) {
 				Log.e(TAG, "connection lost", reason);
-				if (!settings.getBoolean("isConnecting", false)) {
-					connected = false;
-					notifyAlert(
-							getString(R.string.auto_reconnected) + " "
-									+ df.format(new Date()),
-							getString(R.string.reconnect_fail),
-							Notification.FLAG_AUTO_CANCEL);
-					stopSelf();
-				}
-				return;
+				if (settings.getBoolean("isConnecting", false))
+					return;
 			} else if (reason.getMessage().contains(
 					"Closed due to user request")) {
 				Log.e(TAG, "connection lost", reason);
