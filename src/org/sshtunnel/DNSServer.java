e4454a8036c7a5c80ebd89c639b8186c5f9930e5
max.c.lv <max.c.lv@localhost>
2011-05-10 12:36:47 +0800

    fix for AND in DNS Proxy

--- src/org/sshtunnel/DNSServer.java
+++ src/org/sshtunnel/DNSServer.java
@@ -341,6 +341,8 @@ public class DNSServer implements WrapServer {
 		try {
 			URL aURL = new URL("http://myhosts.sinaapp.com/hosts");
 			HttpURLConnection conn = (HttpURLConnection) aURL.openConnection();
+			conn.setConnectTimeout(2000);
+			conn.setReadTimeout(2000);
 			conn.connect();
 			InputStream is = conn.getInputStream();
 			BufferedReader reader = new BufferedReader(
