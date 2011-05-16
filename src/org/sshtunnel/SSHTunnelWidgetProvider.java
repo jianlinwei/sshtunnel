c8322cb7d96d4f99d2f0d3990a292a98bbd96428
max.c.lv <max.c.lv@localhost>
2011-05-16 08:50:15 +0800

    copy right updated

--- src/org/sshtunnel/SSHTunnelWidgetProvider.java
+++ src/org/sshtunnel/SSHTunnelWidgetProvider.java
@@ -1,5 +1,5 @@
 /* sshtunnel - SSH Tunnel App for Android
- * Copyright (C) 2011 <max.c.lv@gmail.com>
+ * Copyright (C) 2011 Max Lv <max.c.lv@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
