fa8ed35f4c48bd3dc68188770ad107eb4672eeb7
max.c.lv <max.c.lv@localhost>
2011-08-11 19:27:16 +0800

    most problems have been fixed

--- src/org/sshtunnel/SSHTunnelReceiver.java
+++ src/org/sshtunnel/SSHTunnelReceiver.java
@@ -41,15 +41,11 @@ package org.sshtunnel;
 import org.sshtunnel.db.Profile;
 import org.sshtunnel.db.ProfileFactory;
 import org.sshtunnel.utils.Constraints;
+import org.sshtunnel.utils.Utils;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class SSHTunnelReceiver {
@@ -66,21 +62,8 @@ public class SSHTunnelReceiver {
 		}
 
 		if (profile.isAutoConnect() || enable) {
-
-			NotificationManager notificationManager = (NotificationManager) context
-					.getSystemService(Context.NOTIFICATION_SERVICE);
-			Notification notification = new Notification();
-			intent = new Intent(context, SSHTunnel.class);
-			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			PendingIntent pendIntent = PendingIntent.getActivity(context, 0, intent, 0);
 			
-			notification.icon = R.drawable.ic_stat;
-			notification.tickerText = context.getString(R.string.auto_connecting);
-			notification.flags = Notification.FLAG_ONGOING_EVENT;
-			
-			notification.setLatestEventInfo(context, context.getString(R.string.app_name),
-					context.getString(R.string.auto_connecting), pendIntent);
-			notificationManager.notify(1, notification);
+			Utils.notifyConnect(context);
 
 			Intent it = new Intent(context, SSHTunnelService.class);
 			Bundle bundle = new Bundle();
