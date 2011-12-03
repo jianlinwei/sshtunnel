4609cfe4ebc472e723ce9a33dc519c2eff251e9c
max.c.lv <max.c.lv@localhost>
2011-12-03 09:25:02 +0800

    1.7.4beta

--- assets/ssh.sh
+++ assets/ssh.sh
@@ -5,12 +5,12 @@ DIR=/data/data/org.sshtunnel.beta
 case $1 in
   dynamic)
 
-$DIR/sshtunnel -i /data/data/org.sshtunnel.beta/.ssh/private_key -CNT -p $2 -D $3 -L 127.0.0.1:5353:8.8.8.8:53 $4@$5
+$DIR/sshtunnel -C -c arcfour,blowfish-cbc,3des-cbc -i /data/data/org.sshtunnel.beta/.ssh/private_key -CNT -p $2 -D $3 -L 127.0.0.1:5353:8.8.8.8:53 $4@$5
   
   ;;
   local)
 
-$DIR/sshtunnel -i /data/data/org.sshtunnel.beta/.ssh/private_key -CNT -p $2 -L 127.0.0.1:$3:$4:$5 -L 127.0.0.1:5353:8.8.8.8:53 $6@$7
+$DIR/sshtunnel -C -c arcfour,blowfish-cbc,3des-cbc -i /data/data/org.sshtunnel.beta/.ssh/private_key -CNT -p $2 -L 127.0.0.1:$3:$4:$5 -L 127.0.0.1:5353:8.8.8.8:53 $6@$7
   
   ;;
 esac
